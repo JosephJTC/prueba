f631352fa92fcf5588833ad66c3bb069b1abfa37
JosephJTC <kayra.naoten@gmail.com>
2026-08-22 22:57:22 -0600

    Update rectangle size in game.js
    Increased the size of the red rectangle drawn on the canvas.

--- game.js
+++ game.js
@@ -1,10 +1,7 @@
+console.log("JavaScript funcionando");
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+
 ctx.fillStyle = "red";
-ctx.fillRect(100, 100, 50, 50);
-ctx.beginPath();
-
-ctx.moveTo(100, 200);
-ctx.lineTo(300, 200);
-
-ctx.stroke();
+ctx.fillRect(100, 100, 100, 100);
