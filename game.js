c4bda521d9a0e5355b80964cebe1ed211e9f2fc6
JosephJTC <kayra.naoten@gmail.com>
2026-08-22 22:55:39 -0600

    Update game.js

--- game.js
+++ game.js
@@ -2,3 +2,9 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "red";
 ctx.fillRect(100, 100, 50, 50);
+ctx.beginPath();
+
+ctx.moveTo(100, 200);
+ctx.lineTo(300, 200);
+
+ctx.stroke();
